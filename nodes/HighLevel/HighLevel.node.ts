84639d1342b7d704e2dfe6292f37e72fc7e9d88b
Stamsy <stanimira.rikova@digital-boss.de>
2024-09-17 16:31:59 +0300

    keep only v2 of the node

--- nodes/HighLevel/HighLevel.node.ts
+++ nodes/HighLevel/HighLevel.node.ts
@@ -1,25 +1,72 @@
-import type { INodeTypeBaseDescription, IVersionedNodeType } from 'n8n-workflow';
-import { VersionedNodeType } from 'n8n-workflow';
+import { INodeType, INodeTypeDescription } from 'n8n-workflow';
+import { contactFields, contactNotes, contactOperations } from './description/ContactDescription';
+import { opportunityFields, opportunityOperations } from './description/OpportunityDescription';
+import { taskFields, taskOperations } from './description/TaskDescription';
+import { calendarFields, calendarOperations } from './description/CalendarDescription';
+import { getContacts, getPipelines, getPipelineStages, getTimezones, getUsers } from './GenericFunctions';
 
-import { HighLevelV1 } from './v1/HighLevelV1.node';
-import { HighLevelV2 } from './v2/HighLevelV2.node';
+export class HighLevel implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'HighLevel',
+		name: 'highLevel',
+		icon: 'file:highLevel.svg',
+		group: ['transform'],
+		version: 1,
+		description: 'Consume HighLevel API',
+		subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}',
+		defaults: {
+			name: 'HighLevel',
+		},
+		inputs: ['main'],
+		outputs: ['main'],
+		credentials: [
+			{
+				name: 'highLevelOAuth2Api',
+				required: true,
+			},
+		],
+		requestDefaults: {
+			baseURL: '={{ $credentials.baseUrl }}',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+				Version: '2021-07-28',
+			},
+		},
+		properties: [
+			{
+				displayName: 'Resource',
+				name: 'resource',
+				type: 'options',
+				noDataExpression: true,
+				options: [
+					{ name: 'Contact', value: 'contact' },
+					{ name: 'Opportunity', value: 'opportunity' },
+					{ name: 'Task', value: 'task' },
+					{ name: 'Calendar', value: 'calendar' },
+				],
+				default: 'contact',
+				required: true,
+			},
+			...contactOperations,
+			...contactNotes,
+			...contactFields,
+			...opportunityOperations,
+			...opportunityFields,
+			...taskOperations,
+			...taskFields,
+			...calendarOperations,
+			...calendarFields,
+		],
+	};
 
-export class HighLevel extends VersionedNodeType {
-	constructor() {
-		const baseDescription: INodeTypeBaseDescription = {
-			displayName: 'HighLevel',
-			name: 'highLevel',
-			icon: 'file:highLevel.svg',
-			group: ['transform'],
-			defaultVersion: 2,
-			description: 'Consume HighLevel API',
-		};
-
-		const nodeVersions: IVersionedNodeType['nodeVersions'] = {
-			1: new HighLevelV1(baseDescription),
-			2: new HighLevelV2(baseDescription),
-		};
-
-		super(nodeVersions, baseDescription);
-	}
+	methods = {
+		loadOptions: {
+			getContacts,
+			getPipelines,
+			getPipelineStages,
+			getUsers,
+			getTimezones,
+		},
+	};
 }
